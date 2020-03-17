ee3e75af79adfa938560a62b38c7ab5ead341f0c
tpe19002090225 <1921106953@qq.com>
2020-03-17 15:14:27 +0800

    just3

---
+++ stat.js
@@ -0,0 +1,20 @@
+(function(){
+    function reduce(array, combine, start) {
+    var current = start;
+        for (var element of array) {
+        current = combine(current, element);
+            }
+            return current;
+} 
+var max=function(data){
+    return  reduce(
+        data,
+        function(a,b){
+            return a>b.credit?a:b.credit;
+        },0)
+    
+}
+window.my={"max":max}
+})()
+
+
